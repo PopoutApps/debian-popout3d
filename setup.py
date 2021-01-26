c6e8798ebba02ab89c57bee34ad8c612bedc6c57
PopoutApps <77306008+PopoutApps@users.noreply.github.com>
2021-01-26 09:26:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     long_description="Take a set of photos of a subject and the software will create a 3D image from each pair of photos in the set, so that you can choose the best. Vertical and rotational alignment of the left and right images is essential for a convincing 3D effect, but difficult to achieve with an ordinary camera. The software corrects any misalignment. Formats available are anaglyph (red/cyan), side-by-side and crossover. Several sets of photos can be processed at once, they can be previewed and poor ones can be deleted.",
     license="GNU GENERAL PUBLIC LICENSE GPLv3",
     keywords="graphics",
-    url="https://github.com/debian-popout3d",
+    url="https://github.com/PopoutApps/debian-popout3d",
 )
 
